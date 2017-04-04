c3050b98a7e90349e4533f62502fdbb585f5c311
DanHederman <c15410232@mydit.ie>
2017-04-04 19:05:25 +0100

    Updated and reformatted GUI prototype

---
+++ OneDrive - Dublin Institute of Technology/College/2nd_Year/Semester-2/Assignment1/src/com/company/AbusiveLanguageDetector.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import javax.swing.*;
+
+/**
+ * Created by danhe on 04/04/2017.
+ */
+public class AbusiveLanguageDetector {
+    private JButton addToAbusiveDictionaryButton;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton checkFileButton;
+}
