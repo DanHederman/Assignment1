c761fbdc317a4b64f08bbd49c1453444c68d7b94
DanHederman <c15410232@mydit.ie>
2017-04-11 22:49:55 +0100

    New gui class that was built using intellijs form plugin

--- OneDrive - Dublin Institute of Technology/College/2nd_Year/Semester-2/Assignment1/src/com/company/AbusiveLanguageDetector.java
+++ OneDrive - Dublin Institute of Technology/College/2nd_Year/Semester-2/Assignment1/src/com/company/AbusiveLanguageDetector.java
@@ -1,13 +1,66 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
 /**
  * Created by danhe on 04/04/2017.
  */
-public class AbusiveLanguageDetector {
+public class AbusiveLanguageDetector extends JFrame implements ActionListener {
     private JButton addToAbusiveDictionaryButton;
     private JTextField textField2;
     private JTextField textField3;
     private JButton checkFileButton;
+    public JPanel panelMain;
+    FileWriter fWriter = null;
+    BufferedWriter writer = null;
+
+    public JPanel getPanelMain() {
+        return panelMain;
+    }
+
+    public void setPanelMain(JPanel panelMain) {
+        this.panelMain = panelMain;
+    }
+
+
+        //Action performed to control what happens when a button is pressed.
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == addToAbusiveDictionaryButton) {
+
+            JTextField textField = textField2; //
+            String text = textField.getText();
+
+
+            try {
+                fWriter = new FileWriter("abuse.txt", true);
+                writer = new BufferedWriter(fWriter);
+                writer.write(String.valueOf(text));
+                writer.newLine();
+                writer.close();
+                System.err.println("Your input of " + text.length() + " characters was saved.");
+            } catch (Exception c) {
+                System.out.println("Error!");
+            }
+
+        }
+
+        if (e.getSource() == checkFileButton)
+
+        {
+            JOptionPane.showMessageDialog(this, "Abuse.txt");
+            FileManager file = new FileManager("abuse.txt");
+            file.connectToFile();
+            System.out.println("\n");
+            for (int i = 0; i < 1; i++) {
+                System.out.print(file.readFile()[i]);
+            }
+        }
+    }
 }
+
--- OneDrive - Dublin Institute of Technology/College/2nd_Year/Semester-2/Assignment1/src/com/company/Main.java
+++ OneDrive - Dublin Institute of Technology/College/2nd_Year/Semester-2/Assignment1/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import javax.swing.*;
+
 /**
  * Created by danhe on 01/03/2017.
  */
@@ -7,7 +9,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        GUI B1 = new GUI("Abusive Language");
+        JFrame abuse = new JFrame("Abusive Text Detector");
+        abuse.setContentPane(new AbusiveLanguageDetector().panelMain);
+        abuse.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        abuse.pack();
+        abuse.setVisible(true);
 
     }
 
