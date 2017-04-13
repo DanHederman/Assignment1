a0249ab2ab91b762772ca35bb2e6be1ed8d89f4c
DanHederman <c15410232@mydit.ie>
2017-04-13 02:03:53 +0100

    The program now counts each individual abusive word even if there are multiple abusive words in the same line A large number of comments were added as well as some variable name changes

--- OneDrive - Dublin Institute of Technology/College/2nd_Year/Semester-2/Assignment1/src/com/company/GUI.java
+++ OneDrive - Dublin Institute of Technology/College/2nd_Year/Semester-2/Assignment1/src/com/company/GUI.java
@@ -9,7 +9,21 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 /**
- * Created by danhe on 12/04/2017.
+ Author: Dan Hederman
+
+ Start Date: 20/3/2017
+
+ Interpreter: Intellij
+
+ For my second year semester 2 Java Assignment in object orientated programmingI choose to
+ code an abusive content detecting program. The idea here is to have a text file full of
+ abusive words. The user will input the name of a file that will be checked against the
+ file full of abusive words. The user will be able to enter new abusive words into the
+ text file full of abusive words.
+
+ intellij has a built in gui form creator which I will use as well as
+
+
  */
 public class GUI {
     private JButton button1;
@@ -19,7 +33,6 @@ public class GUI {
     private JTextField textField2;
     private JTextArea textArea1;
     public JPanel panelMain;
-    File file;
     FileWriter fWriter = null;
     BufferedWriter writer = null;
     Scanner scan = null;
@@ -35,9 +48,13 @@ public class GUI {
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
+                //Used to read in the new abusive word from the textfield into the abusive words file
+
                 JTextField textField = textField1; //
                 String text = textField.getText();
 
+                //Try catch to append the new abusive word to the end of the abusive words file
 
                 try {
                     fWriter = new FileWriter("abuse.txt", true);
@@ -58,55 +75,79 @@ public class GUI {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                //Reads in the users file of choice to be checked for abusive words
+
                 JTextField textField = textField2;
                 String text2 = textField.getText();
 
 
                 File file = new File("C:\\Users\\danhe\\OneDrive - Dublin Institute of Technology\\College\\2nd_Year\\Semester-2\\Assignment1\\abuse.txt");
+
+                //By using the +text2 I allow the user to input the file name only without the extension
+
                 File file2 = new File("C:\\Users\\danhe\\OneDrive - Dublin Institute of Technology\\College\\2nd_Year\\Semester-2\\Assignment1\\" + text2 + ".txt");
+
+                //First scanner to read the abusive text file
 
                 try {
                     scan = new Scanner(file);
                 } catch (FileNotFoundException e1) {
                     e1.printStackTrace();
                 }
+
+                //File scanner to read the text file to be checked for abusive words
+
                 try {
                     scan2 = new Scanner(file2);
                 } catch (FileNotFoundException e1) {
                     e1.printStackTrace();
                 }
 
-                //This array will have alll the words to check against the text file
+                //This array will have all the words to check against the text file
 
-                ArrayList<String> dictionary = new ArrayList<>();
+                ArrayList<String> abusivedictionary = new ArrayList<>();
 
                 //The hashmap will have the number of times each abusive word is in the file to be checked
 
                 HashMap<String,Integer> hits = new HashMap<String, Integer>();
+
+                //While loop to read the the abusive text file into the array abusivedictionary
+
                 while(scan.hasNextLine())
                 {
                     str = scan.nextLine();
-                    dictionary.add(str);
+                    abusivedictionary.add(str);
                     hits.put(str, 0);
                 }
+
+                //While loop to scan the file to be checked for abusive words
+
                 while (scan2.hasNextLine())
                 {
                     str2 = scan2.nextLine();
-                    System.out.println(str2);
-                    for(String str: dictionary)
-                    {
-                        if(str2.contains(str))
-                        {
-                            hits.put(str, hits.get(str) + 1);
+
+                    //For loop to split the line of text into individual words
+
+                    for (String s2: str2.split(" ")) {
+
+                        //Nested if loop inside a for loop to count the number of each abusive word in the file
+
+                        for (String str : abusivedictionary) {
+                            if (s2.contains(str)) {
+                                hits.put(str, hits.get(str) + 1);
+                            }
                         }
                     }
                 }
-                for(String str: dictionary)
+
+                //For loop to print out a message box to the user the number of each abusive word in the text file
+
+                for(String str: abusivedictionary)
                 {
-                    System.out.println(str + " appeared " + hits.get(str) + " times");
+                    String output = (str + " appeared " + hits.get(str) + " times");
+                    JOptionPane.showMessageDialog(null, output);
                 }
             }
-
         });
 
         //Action listener to display the contents of the abuse dictionary
@@ -115,29 +156,39 @@ public class GUI {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                BufferedReader in = null;
+                //Set the buffered reader abuse to null
+
+                BufferedReader abuse  = null;
+
+                //Try catch to read the abuse text file into the variable abuse
+
                 try {
-                    in = new BufferedReader(new FileReader("abuse.txt"));
+                    abuse = new BufferedReader(new FileReader("abuse.txt"));
                 } catch (FileNotFoundException e1) {
                     e1.printStackTrace();
                 }
+
                 String line = null;
+
+                //Try catch to read the abuse file line by line
+
                 try {
-                    line = in.readLine();
+                    line = abuse.readLine();
                 } catch (IOException e1) {
                     e1.printStackTrace();
                 }
+
+                //While loop to print the contents of the abuse file to the textarea for the user to see
+
                 while(line != null){
                     textArea1.append(line + "\n");
                     try {
-                        line = in.readLine();
+                        line = abuse.readLine();
                     } catch (IOException e1) {
                         e1.printStackTrace();
                     }
                 }
-
             }
         });
     }
-
 }
